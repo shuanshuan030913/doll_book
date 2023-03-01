01c701f48e9d76f10610f747ad6f840dac006bc0
shuanshuan030913 <skypainter59768@gmail.com>
2023-03-01 22:27:57 +0800

    feat(constants): add constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,13 @@
 /// 狀態選項
-final List<String> statusOptions = ['數調中', '已填單', '大貨中', '已回家'];
+final List<String> statusOptions = ['還在徵', '數調中', '已填單', '大貨中', '已回家'];
 
 /// 列表篩選
-final List<String> filterOptions = ['不限', '已回家', '大貨中', '已填單', '數調中', '已出清'];
+final List<String> filterOptions = [
+  '不限',
+  '已回家',
+  '大貨中',
+  '已填單',
+  '數調中',
+  '還在徵',
+  '已出清'
+];
